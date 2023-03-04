d470783fdc86d0a194aee597f1ad2df61d6b08c0
Olympia Apollo <tfv3@txstate.edu>
2023-03-04 01:01:08 +0000

    submit2

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -21,7 +21,7 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        if (bool) {
+        if(bool){
             return x;
         } else {
             return y;
