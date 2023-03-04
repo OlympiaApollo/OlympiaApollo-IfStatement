3e98c71c8a77a3b8e669ae3dca018b25ad4afad4
Olympia Apollo <tfv3@txstate.edu>
2023-03-04 00:59:21 +0000

    submit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import javax.swing.text.html.HTMLDocument.BlockElement;
 
 public class Lab {
     /**
@@ -20,6 +21,10 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        if (bool) {
+            return x;
+        } else {
+            return y;
+        }
     }
 }
